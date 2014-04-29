74d48032ccb55d70a11da6b754f4ca94e2c947de
Pierce Darragh <pierce.darragh@utah.edu>
2014-04-29 17:59:36 -0600

    Updated usage information
    Now provides legitimate usage information with ‘-h’.

--- src/parse_options.py
+++ src/parse_options.py
@@ -1,23 +1,63 @@
 import argparse
 import sys
 
-def usage (name):
-    '''Gives useful usage information.
+def version (options):
+    '''Prints the version information.
     '''
 
-    print "This is some helpful information.  Maybe."
+    print "{name}, version {version}\n".format(name=options['long_name'],
+                                               version=options['version'])
+
+def usage (options):
+    '''Usage information.
+    '''
+
+    version (options)
+
+    print '''\
+usage: {} [-hvn] [-l log] [-u user]
+         {} {} {}
+
+Modify access to the TCC database services.
+
+    h : prints this help message
+    v : prints the version information
+    n : prevents logs from being written to file and enables console output
+
+    l log  : use 'log' as the logging output location
+    u user : change settings for 'user'
+
+'ACTION'
+    add    : add applications to the service
+    remove : remove applications from the service
+
+'SERVICE'
+    contacts      : modifies access to Address Book
+    icloud        : modifies access to iCloud services (not often used)
+    accessibility : modifies access to system Accessibility settings
+
+    Note that modifying the Accessibility settings requires root access.
+
+'APPLICATIONS'
+    The names of applications can be specified in these ways:
+
+    shortname   : e.g. 'safari'                   'myapp'
+    bundle ID   : e.g. 'com.apple.Safari'         'com.mystuff.MyApp'
+    bundle path : e.g. '/Applications/Safari.app' '/Path/to/MyApp.app'
+
+    You can modify settings for multiple applications at a time.\
+'''.format(options['name'], '{action}', '{service}', '{applications}')
+    sys.exit(0)
 
 def parse (options):
     '''Parses the options given to the script.
     '''
 
-    parser = argparse.ArgumentParser(description="Add or remove applications from the various TCC databases: Contacts, Accessbility, and iCloud.",
-                        add_help=False)
+    parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-h', '--help',
                         action='store_true')
     parser.add_argument('-v', '--version',
-                        action='version',
-                        version=options['long_name'] + ' ' + options['version'])
+                        action='store_true')
     parser.add_argument('-n', '--no-log',
                         action='store_true')
     parser.add_argument('-l', '--log')
@@ -36,17 +76,22 @@ def parse (options):
     args = parser.parse_args()
 
     if args.help:
-        usage(options['name'])
+        usage(options)
+    if args.version:
+        version(options)
         sys.exit(0)
+    options['log'] = not args.no_log
+    options['log_dest'] = args.log
+    options['user'] = args.user
+    if not args.action:
+        print "Error: You must specify an action!"
+        sys.exit(5)
     options['action'] = args.action
     if not args.service:
-        print "You must specify a service!"
+        print "Error: You must specify a service!"
         sys.exit(5)
     options['service'] = args.service
     if not args.apps:
-        print "You must specify at least one application!"
+        print "Error: You must specify at least one application!"
         sys.exit(5)
     options['apps'] = args.apps
-    options['log_dest'] = args.log
-    options['log'] = not args.no_log
-    options['user'] = args.user
