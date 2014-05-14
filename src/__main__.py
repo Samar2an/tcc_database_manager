318b3f2997305bf0829d40482aeefc4cf64fb510
Pierce Darragh <pierce.darragh@utah.edu>
2014-05-14 09:57:36 -0600

    Fixed typo
    Forgot to update version information in ff90908

--- src/__main__.py
+++ src/__main__.py
@@ -21,7 +21,7 @@ def set_globals ():
     options = {}
     options['long_name'] = 'TCC Database Manager'
     options['name'] = '_'.join(options['long_name'].lower().split())
-    options['version'] = '3.1.2'
+    options['version'] = '3.1.3'
 
 def setup_logger ():
     '''Creates the logger to be used throughout.
